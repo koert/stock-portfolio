7bf818cfa4d134630cd0b437bfc0d356aee9beb4
Koert Zeilstra <koert@zencode.nl>
2020-02-23 10:17:17 +0100

    Added article about pure unit test.

--- spring-boot/src/main/angular/src/app/portfolio/portfolio.component.spec.ts
+++ spring-boot/src/main/angular/src/app/portfolio/portfolio.component.spec.ts
@@ -7,11 +7,12 @@ import {ButtonModule} from "primeng/button";
 import {DialogModule} from "primeng/dialog";
 import {DropdownModule} from "primeng/dropdown";
 import {PanelModule} from "primeng/panel";
-import {instance, mock} from "ts-mockito";
-import {StockService} from "../stock.service";
+import {instance, mock, when} from "ts-mockito";
+import {StockLatestPriceResponse, StockService} from "../stock.service";
 import {FormsModule} from "@angular/forms";
 import {TableModule} from "primeng/table";
 import {CalendarModule} from "primeng/calendar";
+import {of} from "rxjs";
 
 describe('PortfolioComponent', () => {
   let component: PortfolioComponent;
@@ -38,5 +39,24 @@ describe('PortfolioComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+
+    expect(component.portfolioRows.length).toBe(2);
   });
+
+  it('refreshPortfolioPrices', () => {
+    let response0 = new StockLatestPriceResponse();
+    response0.latestPrice = 1.23;
+    when(stockService.getStockLatestPrice("AAPL")).thenReturn(of(response0));
+    let response1 = new StockLatestPriceResponse();
+    response1.latestPrice = 2.34;
+    when(stockService.getStockLatestPrice("GOOG")).thenReturn(of(response1));
+
+    component.ngOnInit();
+    component.refreshPortfolioPrices();
+
+    expect(component.portfolioRows.length).toBe(2);
+    expect(component.portfolioRows[0].latestPrice).toBe(1.23);
+    expect(component.portfolioRows[1].latestPrice).toBe(2.34);
+  });
+
 });
